cb83d5b28b94d32e992ee8c60371007099e3df92
ottebrut <ott.andrey.ao@gmail.com>
2023-01-06 14:15:25 +0400

    add usePersonData hook

--- src/containers/person-page/index.tsx
+++ src/containers/person-page/index.tsx
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
-import { SWAPI_PERSON } from "src/constants/api";
-import {
-  IPersonApiResponse,
-  IPersonInfo,
-} from "src/containers/person-page/models";
 import PersonInfo from "src/containers/person-page/person-info";
 import PersonPhoto from "src/containers/person-page/person-photo";
+import usePersonData from "src/containers/person-page/use-person-data";
 import withErrorApi, {
   WithErrorApiViewProps,
 } from "src/hoc-helpers/with-error-api";
 import { getPersonImage } from "src/utils/get-person-data";
-import { getApiResponse } from "src/utils/network";
 
 import styles from "./styles.module.scss";
 
@@ -21,31 +16,7 @@ interface PersonPageProps extends WithErrorApiViewProps {}
 const PersonPage: React.FC<PersonPageProps> = ({ setErrorApi }) => {
   const { id } = useParams();
   const personImageSrc = getPersonImage(id!);
-
-  const [personInfo, setPersonInfo] = useState<IPersonInfo | null>(null);
-  const [personName, setPersonName] = useState<string | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getApiResponse<IPersonApiResponse>(SWAPI_PERSON(id!));
-      if (res) {
-        setPersonInfo([
-          { label: "Height", data: res.height },
-          { label: "Width", data: res.width },
-          { label: "Hair Color", data: res.hair_color },
-          { label: "Skin Color", data: res.skin_color },
-          { label: "Eye Color", data: res.eye_color },
-          { label: "Birth Year", data: res.birth_year },
-          { label: "Gender", data: res.gender },
-        ]);
-        setPersonName(res.name);
-
-        setErrorApi(false);
-      } else {
-        setErrorApi(true);
-      }
-    })();
-  }, [id, setErrorApi]);
+  const { personInfo, personName } = usePersonData({ id: id!, setErrorApi });
 
   return (
     <div className={styles.person}>
---
+++ src/containers/person-page/use-person-data.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+import { SWAPI_PERSON } from "src/constants/api";
+import {
+  IPersonApiResponse,
+  IPersonInfo,
+} from "src/containers/person-page/models";
+import { WithErrorApiViewProps } from "src/hoc-helpers/with-error-api";
+import { getApiResponse } from "src/utils/network";
+
+interface UsePersonDataProps extends WithErrorApiViewProps {
+  id: string;
+}
+
+export default function usePersonData({
+  id,
+  setErrorApi,
+}: UsePersonDataProps): {
+  personInfo: IPersonInfo | null;
+  personName: string | null;
+} {
+  const [personInfo, setPersonInfo] = useState<IPersonInfo | null>(null);
+  const [personName, setPersonName] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getApiResponse<IPersonApiResponse>(SWAPI_PERSON(id!));
+      if (res) {
+        setPersonInfo([
+          { label: "Height", data: res.height },
+          { label: "Width", data: res.width },
+          { label: "Hair Color", data: res.hair_color },
+          { label: "Skin Color", data: res.skin_color },
+          { label: "Eye Color", data: res.eye_color },
+          { label: "Birth Year", data: res.birth_year },
+          { label: "Gender", data: res.gender },
+        ]);
+        setPersonName(res.name);
+
+        setErrorApi(false);
+      } else {
+        setErrorApi(true);
+      }
+    })();
+  }, [id, setErrorApi]);
+
+  return {
+    personInfo,
+    personName,
+  };
+}
